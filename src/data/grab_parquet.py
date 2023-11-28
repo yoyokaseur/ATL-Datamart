a87f43d7c09782bc771b22a98c718e2a8349f970
Yohan <yohan.henry@epitech.eu>
2023-11-28 12:12:03 +0100

    [MOD] save the parquet file in the minio bucket server

--- src/data/grab_parquet.py
+++ src/data/grab_parquet.py
@@ -3,9 +3,14 @@ import urllib.request
 import pandas as pd
 import sys
 
+prefix_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data' #_2018-01.parquet'
+
 def main():
     grab_data()
-    
+    return 0
+
+def get_filename(years : int, month : int):
+    return f"yellow_tripdata_{years}-{'0' if month < 10 else ''}{month}.parquet"
 
 def grab_data() -> None:
     """Grab the data from New York Yellow Taxi
@@ -15,9 +20,16 @@ def grab_data() -> None:
     Files need to be saved into "../../data/raw" folder
     This methods takes no arguments and returns nothing.
     """
+    for years in range(2023, 2024):
+        for month in range(1, 13):
+            filename = get_filename(years, month)
+            url = f"{prefix_url}/{filename}"
+            req = urllib.request.Request(url=url)
+            with urllib.request.urlopen(req) as data:
+                write_data_minio(filename, data)
 
 
-def write_data_minio():
+def write_data_minio(filename : str, data):
     """
     This method put all Parquet files into Minio
     Ne pas faire cette méthode pour le moment
@@ -28,12 +40,13 @@ def write_data_minio():
         access_key="minio",
         secret_key="minio123"
     )
-    bucket: str = "NOM_DU_BUCKET_ICI"
+    bucket: str = "datalake"
     found = client.bucket_exists(bucket)
     if not found:
         client.make_bucket(bucket)
     else:
         print("Bucket " + bucket + " existe déjà")
+    client.put_object(bucket, filename, data, length=-1, part_size=10*1024 * 1024)
 
 if __name__ == '__main__':
     sys.exit(main())
